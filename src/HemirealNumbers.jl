5d545f2ce85f423ff4e497240170efc7ff2059d8
Tim Holy <tim.holy@gmail.com>
2015-12-06 12:35:53 -0600

    Initial working commit

--- src/HemirealNumbers.jl
+++ src/HemirealNumbers.jl
@@ -1,5 +1,135 @@
 module HemirealNumbers
 
-# package code goes here
+import Base: +, -, *, /, \, ^, abs, abs2, conj, convert, isfinite, promote_op, promote_rule, real, show, zero
+import Base.LinAlg: A_mul_Bt, A_mul_Bc, At_mul_B, Ac_mul_B, At_mul_Bt, Ac_mul_Bc
+
+export PureHemi, Hemireal, μ, ν
+
+immutable PureHemi{T<:Real} <: Number
+    m::T
+    n::T
+end
+PureHemi(m::Real, n::Real) = PureHemi(promote(m,n)...)
+
+immutable Hemireal{T<:Real} <: Number
+    r::T
+    h::PureHemi{T}
+end
+
+Hemireal(r::Real, m::Real, n::Real) = Hemireal(r, PureHemi(m, n))
+function Hemireal{R,H}(r::R, h::PureHemi{H})
+    T = promote_type(R,H)
+    Hemireal{T}(r, PureHemi{T}(h.m, h.n))
+end
+Hemireal{R}(h::PureHemi{R}) = Hemireal(zero(R), h)
+Hemireal{R}(r::R) = Hemireal(r, zero(PureHemi{R}))
+
+const μ = PureHemi(true,false)
+const ν = PureHemi(false,true)
+
+## PureHemi implementation
+convert{R}(::Type{PureHemi{R}}, x::PureHemi) = PureHemi{R}(x.m, x.n)
+convert{R}(::Type{PureHemi{R}}, x::Real) = x == 0 ? zero(PureHemi{R}) : throw(DomainError())  # error("Non-zero reals cannot be converted to pure-hemi numbers")
+
+(-)(x::PureHemi) = PureHemi(-x.m, -x.n)
+
+(+)(x::PureHemi, y::PureHemi) = PureHemi(x.m+y.m, x.n+y.n)
+(-)(x::PureHemi, y::PureHemi) = PureHemi(x.m-y.m, x.n-y.n)
+(*)(x::PureHemi, y::PureHemi) = x.m*y.n + x.n*y.m
+(*)(c::Bool, x::PureHemi)     = PureHemi(c*x.m, c*x.n)
+(*)(c::Real, x::PureHemi)     = PureHemi(c*x.m, c*x.n)
+(*)(x::PureHemi, c::Real)     = c*x
+(/)(x::PureHemi, c::Real)     = PureHemi(x.m/c, x.n/c)
+(\)(c::Real, x::PureHemi)     = x/c
+function (/)(x::PureHemi, y::PureHemi)
+    @assert x.m*y.n == x.n*y.m
+    y.n == 0 ? x.m/y.m : x.n/y.n
+end
+(\)(x::PureHemi, y::PureHemi) = y/x
+(^)(x::PureHemi, p::Integer)  = (x*x)^(p/2)  # note x*x is much faster than x^2
+(^)(x::PureHemi, p::Rational) = (x*x)^(p/2)
+(^)(x::PureHemi, p::Real)     = (x*x)^(p/2)
+
+# Symmetric division (there are other solutions y to y*x = c)
+(/)(c::Real, x::PureHemi) = PureHemi(c/(2*x.n), c/(2*x.m))
+(\)(x::PureHemi, c::Real) = c/x
+
+real{R}(::Type{PureHemi{R}}) = R
+real{R}(x::PureHemi{R}) = zero(R)
+conj(x::PureHemi) = x
+
+zero{R}(::Type{PureHemi{R}}) = PureHemi{R}(0, 0)
+zero{R}(::PureHemi{R}) = zero(PureHemi{R})
+
+isfinite(x::PureHemi) = isfinite(x.m) && isfinite(x.n)
+# I'm not entirely certain whether the next two should even be defined.
+# (note that abs2(x) != x*x)
+abs2(x::PureHemi) = x.m*x.m + x.n*x.n
+abs(x::PureHemi) = sqrt(abs2(x))
+
+## Hemireal implementation
+convert{R}(::Type{Hemireal{R}}, x::Hemireal) = Hemireal{R}(x.r, x.h)
+convert{R}(::Type{Hemireal{R}}, x::PureHemi) = Hemireal{R}(0, x)
+convert{R}(::Type{Hemireal{R}}, x::Real) = Hemireal{R}(x, zero(PureHemi{R}))
+convert{R}(::Type{Hemireal{R}}, r::Real, m::Real, n::Real) = Hemireal{R}(r, PureHemi{R}(m, n))
+
+(-)(x::Hemireal) = Hemireal(-x.r, -x.h)
+
+(+)(x::Hemireal, y::Hemireal) = Hemireal(x.r+y.r, x.h+y.h)
+(-)(x::Hemireal, y::Hemireal) = Hemireal(x.r-y.r, x.h-y.h)
+(*)(x::Hemireal, y::Hemireal) = Hemireal(x.r*y.r + x.h*y.h, x.r*y.h + x.h*y.r)
+
+(+)(x::Hemireal, y::PureHemi) = x+Hemireal(y)
+(+)(x::PureHemi, y::Hemireal) = y+x
+(-)(x::Hemireal, y::PureHemi) = x-Hemireal(y)
+(-)(x::PureHemi, y::Hemireal) = Hemireal(x)-y
+(*)(x::Hemireal, y::PureHemi) = x*Hemireal(y)
+(*)(x::PureHemi, y::Hemireal) = y*x
+
+(+)(x::Hemireal, y::Real) = x+Hemireal(y)
+(+)(x::Real, y::Hemireal) = y+x
+(-)(x::Hemireal, y::Real) = x-Hemireal(y)
+(-)(x::Real, y::Hemireal) = Hemireal(x)-y
+(*)(c::Bool, x::Hemireal) = Hemireal(c*x.r, c*x.h)
+(*)(c::Real, x::Hemireal) = Hemireal(c*x.r, c*x.h)
+(*)(x::Hemireal, c::Real) = c*x
+(/)(x::Hemireal, c::Real) = Hemireal(x.r/c, x.h/c)
+(\)(c::Real, x::Hemireal) = x/c
+
+real{R}(::Type{Hemireal{R}}) = R
+real{R}(x::Hemireal{R}) = x.r
+conj(x::Hemireal) = x
+
+zero{R}(::Type{Hemireal{R}}) = Hemireal{R}(0, PureHemi{R}(0, 0))
+zero{R}(::Hemireal{R}) = zero(Hemireal{R})
+
+isfinite(x::Hemireal) = isfinite(x.r) && isfinite(x.h)
+# ?
+abs2(x::Hemireal) = x.r*x.r + abs2(x.h)
+abs(x::Hemireal) = sqrt(abs2(x))
+
+(*){H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = A_mul_B!(Array(promote_type(real(H1),real(H2)), size(A,1), size(B,2)), A, B)
+A_mul_Bt{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = A_mul_Bt!(Array(promote_type(real(H1),real(H2)), size(A,1), size(B,1)), A, B)
+A_mul_Bc{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = A_mul_Bt!(Array(promote_type(real(H1),real(H2)), size(A,1), size(B,1)), A, B)
+At_mul_B{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = At_mul_B!(Array(promote_type(real(H1),real(H2)), size(A,2), size(B,2)), A, B)
+Ac_mul_B{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = At_mul_B!(Array(promote_type(real(H1),real(H2)), size(A,2), size(B,2)), A, B)
+At_mul_Bt{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = At_mul_Bt!(Array(promote_type(real(H1),real(H2)), size(A,2), size(B,1)), A, B)
+Ac_mul_Bc{H1<:PureHemi,H2<:PureHemi}(A::StridedVecOrMat{H1}, B::StridedVecOrMat{H2}) = At_mul_Bt!(Array(promote_type(real(H1),real(H2)), size(A,2), size(B,1)), A, B)
+
+promote_op{H1,H2}(::Base.MulFun, ::Type{PureHemi{H1}}, ::Type{PureHemi{H2}}) = promote_type(H1,H2)
+promote_op{H1,H2}(::Base.DotMulFun, ::Type{PureHemi{H1}}, ::Type{PureHemi{H2}}) = promote_type(H1,H2)
+
+promote_rule{H}(::Type{Bool}, ::Type{PureHemi{H}}) = Hemireal{promote_type(Bool,H)}
+promote_rule{R,H}(::Type{Irrational{R}}, ::Type{PureHemi{H}}) = Hemireal{promote_type(Irrational{R},H)}
+promote_rule{R<:Real,H}(::Type{R}, ::Type{PureHemi{H}}) = Hemireal{promote_type(R,H)}
+promote_rule{H1,H2}(::Type{PureHemi{H1}}, ::Type{PureHemi{H2}}) = PureHemi{promote_type(H1,H2)}
+promote_rule{H}(::Type{Bool}, ::Type{Hemireal{H}}) = Hemireal{promote_type(Bool,H)}
+promote_rule{R,H}(::Type{Irrational{R}}, ::Type{Hemireal{H}}) = Hemireal{promote_type(Irrational{R},H)}
+promote_rule{R<:Real,H}(::Type{R}, ::Type{Hemireal{H}}) = Hemireal{promote_type(R,H)}
+promote_rule{H1,H2}(::Type{PureHemi{H1}}, ::Type{Hemireal{H2}}) = Hemireal{promote_type(H1,H2)}
+promote_rule{H1,H2}(::Type{Hemireal{H1}}, ::Type{Hemireal{H2}}) = Hemireal{promote_type(H1,H2)}
+
+show(io::IO, x::PureHemi) = x.n >= 0 ? print(io, x.m, "μ + ", x.n, 'ν') : print(io, x.m, "μ - ", abs(x.n), 'ν')
+show(io::IO, x::Hemireal) = x.h.m >= 0 ? print(io, x.r, " + ", x.h) : print(io, x.r, " - ", PureHemi(-x.h.m, x.h.n))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,131 @@
 using HemirealNumbers
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+p1 = @inferred(PureHemi(1,2))
+p2 = PureHemi(1,2)
+p3 = @inferred(PureHemi(1.0,2))
+@test p1 == p2
+@test p1 == p3
+
+@test @inferred(convert(PureHemi{Float64}, p1)) === PureHemi(1.0,2.0)
+@test @inferred(convert(PureHemi{Float64}, 0)) == PureHemi(0.0,0.0)
+@test_throws DomainError convert(PureHemi{Float64}, 1)
+
+@test @inferred(-p1) == PureHemi(-1,-2)
+
+@test @inferred(PureHemi(1,2) + PureHemi(5,3)) === PureHemi(6,5)
+@test @inferred((μ+2ν) + (5μ+3ν)) === 6μ+5ν
+@test PureHemi(1,2) - PureHemi(5,3) == PureHemi(-4,-1)
+@test μ*μ == 0
+@test ν*ν == 0
+@test ν*μ == 1
+@test μ*ν == 1
+@test 2*PureHemi(3,4) == PureHemi(6,8)
+@test PureHemi(3,4)*(-1) == PureHemi(-3,-4)
+@test false*PureHemi(3,4) == 0
+@test PureHemi(3,4)*true == 3μ+4ν
+@test @inferred((3μ+4ν)/2) === 1.5μ+2ν
+@test 2\(3μ+4ν) === 1.5μ+2ν
+@test 18/PureHemi(3,3) == PureHemi(3,3)
+@test PureHemi(3,3)\7 === PureHemi(7/6,7/6)
+@test @inferred((4μ+2ν)/(2μ+ν)) === 2.0
+@test_throws AssertionError (4μ+2ν)/(2μ+3ν)
+@test @inferred((4μ+2ν)\(2μ+ν)) === 0.5
+@test PureHemi(3,4)^2 == 24
+@test PureHemi(3,4)^(1//4) == 24^(1//8)
+@test PureHemi(3,4)^2.0 == 24.0
+@test real(PureHemi(0.2,0.3)) == 0.0
+@test real(PureHemi{Bool}) == Bool
+@test zero(PureHemi(0.2,0.3)) == PureHemi(0,0)
+@test zero(PureHemi{Bool}) == PureHemi(false,false)
+@test conj(3μ+4ν) == 3μ+4ν
+@test isfinite(3μ+4ν)
+@test !isfinite(Inf*μ + 3.2*ν)
+@test_approx_eq PureHemi(1,2) PureHemi(nextfloat(1.0), nextfloat(2.0))
+
+@test @inferred(Hemireal(1,2,3)) === 1+2μ+3ν
+@test @inferred(Hemireal(1.0,2,3)) === 1.0+2.0μ+3.0ν
+@test @inferred(Hemireal(2μ+3ν)) === Hemireal(0,2,3)
+@test @inferred(Hemireal(7.0,2μ+3ν)) === Hemireal(7.0,2.0,3.0)
+@test @inferred(Hemireal(7)) === Hemireal(7,0,0)
+
+@test convert(Hemireal{Float64}, 1) === Hemireal(1.0,0.0,0.0)
+@test convert(Hemireal{Float64}, 2μ+3ν) === Hemireal(0.0,2.0,3.0)
+@test convert(Hemireal{Float64}, Hemireal(1,2,3)) === Hemireal(1.0,2.0,3.0)
+
+@test @inferred(-(7+2μ)) === Hemireal(-7,-2,0)
+@test @inferred((1+2μ+3ν) + (5.5+2.1μ+3.2ν)) === Hemireal(6.5,4.1,6.2)
+@test_approx_eq @inferred((1+2μ+3ν) - (5.5+2.1μ+3.2ν)) Hemireal(-4.5,-0.1,-0.2)
+@test_approx_eq @inferred((1+2μ+3ν) * (5.5+2.1μ+3.2ν)) Hemireal(5.5+6.4+6.3, 13.1, 19.7)
+
+@test @inferred((1+2μ+3ν) + PureHemi(2.1,3.2)) === Hemireal(1,4.1,6.2)
+@test @inferred(PureHemi(7,9) + (2+μ-6ν)) === Hemireal(2,8,3)
+@test_approx_eq @inferred((1+2μ+3ν) - PureHemi(2.1,3.2)) Hemireal(1.0,-0.1,-0.2)
+@test @inferred(PureHemi(7,9) - (2+μ-6ν)) === Hemireal(-2,6,15)
+@test_approx_eq @inferred((1+2μ+3ν) * PureHemi(2.1,3.2)) Hemireal(6.4+6.3, 2.1, 3.2)
+@test @inferred(PureHemi(7,9) * (2+μ-6ν)) === Hemireal(-33,14,18)
+
+@test @inferred((1+2μ+3ν) + 5.5) === Hemireal(6.5,2.0,3.0)
+@test @inferred(3 + (2+μ-6ν)) === Hemireal(5,1,-6)
+@test @inferred((1+2μ+3ν) - 5.5) === Hemireal(-4.5,2.0,3.0)
+@test @inferred(3 - (2+μ-6ν)) === Hemireal(1,-1,6)
+@test @inferred(false*(2+μ-6ν)) === Hemireal(0,0,0)
+@test @inferred((2+μ-6ν)*true) === Hemireal(2,1,-6)
+@test @inferred(1.5*(2+μ-6ν)) === Hemireal(3.0,1.5,-9.0)
+@test @inferred((2+μ-6ν)*1.5) === Hemireal(3.0,1.5,-9.0)
+@test @inferred((2+μ-6ν)/1.5) === Hemireal(2/1.5,1/1.5,-6/1.5)
+@test @inferred(1.5\(2+μ-6ν)) === Hemireal(2/1.5,1/1.5,-6/1.5)
+
+x = 3.2+μ-ν
+@test real(x) === 3.2
+@test real(Hemireal{Float16}) == Float16
+@test conj(x) === x
+@test zero(x) === Hemireal(0.0,0.0,0.0)
+@test zero(Hemireal{Float16}) === Hemireal{Float16}(0,0,0)
+
+a = [μ+2ν, 5μ-ν]
+# @test @inferred(a*a') == [4 9; 9 -10]   # julia #14293
+# @test isa(a*a', Matrix{Int})
+# @test @inferred(a'*a) == [-6]
+A = [μ 0; 2μ+3ν 5μ-4ν]
+@test @inferred(A*a) == [2,-18]
+@test isa(A*a, Vector{Hemireal{Int}})
+z = zero(PureHemi{Int})
+A = [μ z; 2μ+3ν 5μ-4ν]
+# @test @inferred(A*a) == [2,18]           # julia #14293
+# @test isa(A*a, Vector{Int})
+@test @inferred(A*A)   == [0 0; 10 -40]
+@test isa(A*A,   Matrix{Int})
+@test @inferred(A*A')  == [0 3;  3 -28]
+@test isa(A*A',  Matrix{Int})
+@test @inferred(A*A.') == [0 3;  3 -28]
+@test isa(A*A.', Matrix{Int})
+@test @inferred(A'*A)  == [12 7; 7 -40]
+@test isa(A'*A,  Matrix{Int})
+@test @inferred(A.'*A) == [12 7; 7 -40]
+@test isa(A.'*A, Matrix{Int})
+@test @inferred(A'*A') == [0 10; 0 -40]
+@test isa(A'*A', Matrix{Int})
+@test @inferred(A.'*A.') == [0 10; 0 -40]
+@test isa(A.'*A.', Matrix{Int})
+
+@test μ*a == [2,-1]
+@test isa(μ*a, Vector{Int})
+
+# Promotion
+@test isa([μ, false], Vector{Hemireal{Bool}})
+@test isa([2μ, false], Vector{Hemireal{Int}})
+@test isa([μ, π], Vector{Hemireal{Float64}})
+@test isa([1+μ, false], Vector{Hemireal{Int}})
+@test isa([1+μ, π], Vector{Hemireal{Float64}})
+@test isa([1+μ,2.0+ν], Vector{Hemireal{Float64}})
+
+io = IOBuffer()
+show(io, 1μ+2ν)
+@test takebuf_string(io) == "1μ + 2ν"
+show(io, 1μ-2ν)
+@test takebuf_string(io) == "1μ - 2ν"
+show(io, 3-4.0μ+2ν)
+@test takebuf_string(io) == "3.0 - 4.0μ + 2.0ν"
+show(io, 3-4.0μ-2ν)
+@test takebuf_string(io) == "3.0 - 4.0μ - 2.0ν"
